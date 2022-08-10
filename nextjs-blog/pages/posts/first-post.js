17fe736f63b80a2073b0b356703e8cc14586c60c
YoungMin <kgjdudals@naver.com>
2022-08-11 02:49:22 +0900

    style css

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,23 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from "../../components/layout"
+
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+        <meta name="author" content="Chris Mills" />
+        <meta
+          name="description"
+          content="이 페이지는 블로그 페이지입니다."
+          ></meta>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
