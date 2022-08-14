5cac4419ca822d8d6fc0ac478e4771817f6c11b9
YoungMin <kgjdudals@naver.com>
2022-08-14 17:46:25 +0900

    nextjs 기능 학습

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -6,7 +6,25 @@ import utilStyles from '../../styles/utils.module.css';
 import { useRouter } from 'next/router'
 import { useEffect } from 'react';
 
-export async function getStaticProps({ params }) {
+// export async function getStaticProps({ params }) {
+//   const postData = await getPostData(params.id);
+//   return {
+//     props: {
+//       postData,
+//     },
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   // const paths = getAllPostIds();
+//   return {
+//     paths:[{params: {id : "ssr-ssg"}}],
+//     fallback: 'blocking',
+//   };
+// }
+
+export async function getServerSideProps({params, req}) {
+  console.log(`req.cookies: ${JSON.stringify(req.cookies)}`);
   const postData = await getPostData(params.id);
   return {
     props: {
@@ -15,26 +33,18 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export async function getStaticPaths() {
-  // const paths = getAllPostIds();
-  return {
-    paths:[{params: {id : "ssr-ssg"}}],
-    fallback: true,
-  };
-}
-
 export default function Post({postData}) {
   const router = useRouter();
 
-  useEffect(() => {
-    const getText = async () => {
-      const res = await fetch("/api/hello");
-      const data = await res.json();
+  // useEffect(() => {
+  //   const getText = async () => {
+  //     const res = await fetch("/api/hello");
+  //     const data = await res.json();
 
-      alert(data.text)
-    }
-    getText()
-  },[])
+  //     alert(data.text)
+  //   }
+  //   getText()
+  // },[])
 
   if (router.isFallback) {
     return <div>Loading.....</div>
--- nextjs.md
+++ nextjs.md
@@ -32,8 +32,14 @@
 getStaticPaths / remark / dynamic routes
 
 * getStaticPaths : [{params: {id : string}}] 목록을 리턴한다. params 안에 dynamic routes key 가 들어있어야함
+- fallback : 'blocking'
+- blocking 하고 HTML을 generate 하고 보여준다
+- pre-rendering 목록에도 추가됨
 
 * getStaticProps : id 별로 데이터를 읽어서 page에 props를 전달하도록 함
+- 인자 context
+- params
+- return : props / revalidate(ISR로) / notFound: true (fallback: false 에선 무의미)
 
 * Fallback for Dynamic routes
 getStaticPaths 옵션 fallback : true
@@ -46,3 +52,53 @@ router.isFallback
 
 get Static Paths에서 [{params: {id:['a','b','c']}}]로 전달해야함
 
+# Incremental Static Regeneration(ISR)
+
+getStaticProps with revalidate(업데이트 주기 설정)
+
+# getServerSideProps
+
+- 인자 context
+params / req / res / query
+
+request 마다 동작하기 때문에 req.cookies 가능하다.
+
+
+# Layouts
+- 여러 Layout들을 사용할때는 Page.getLayout 활용가능
+
+# Image Component and Optimization
+
+- Improved Performance / Visual Stability / Faster Page Loads / Asset Flexibility
+
+- 디바이스 사이즈 / format / CLS / lazy load / blur placeholder / resizing
+
+# Script Optimization
+
+- import Script from 'next/script'
+- id /src / strategy
+
+# Static File Serving
+
+- public 폴더
+- images / robots.txt / favicon.ico
+
+# Fast Refresh
+
+- React 구성요소일 경우 관련된 컴포넌트 혹은 파일만 update
+- React 외부 요소면 전체 update(full reload)
+- 로컬 상태 유지가 default // @refresh reset
+
+# EsLint
+
+- JS code 에서 문제가 될 부분을 찾고 고칠 수 있는 도구
+- 코드가 지키고 싶은 규칙을 구성할 수 있음
+- 여러 규칙을 합쳐서 나만의 룰 생성 가능
+
+# 환경 변수
+
+- .env.local
+- HOST_NAME=localhost
+- process.env.HOST_NAME
+- HOST=http://$HOST_NAME
+- NEXT_PUBLIC_ * 브라우저에 노출
